593860662290f8559a4bbe74a5b015d13b42e392
Humblefool_01 <kaushikvishwanath010@gmail.com>
2018-05-08 06:31:16 +0530

    Initial commit

---
+++ Assets/Scripts/ballMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ballMovement : MonoBehaviour {
+	public static ballMovement Instance{ set; get;}
+
+	public GameObject ball;
+	public float factor;
+	public GameObject[] whiteHole;
+	public GameObject[] blackHole;
+	public GameObject foreGround;
+	private Rigidbody rb;
+	public static  bool gameOver, finish;
+	public Text text;
+
+	private void Start(){
+		gameOver = false;
+		finish = false;
+		rb = GetComponent<Rigidbody> ();
+	}
+	private void Update(){
+
+		if (gameObject.transform.position.y <= -20f) {
+			foreGround.SetActive (true);
+			text.text = "Game Over!\nPress 'Space' to restart";
+		}
+		if (!gameOver) {
+			foreGround.SetActive (false);
+			rb.AddForce (-Vector3.up * factor);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			SceneManager.LoadScene ("Scene1");
+		}
+	}
+	private void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "blackHole") {
+			int i = Random.Range (0, whiteHole.Length);
+			gameObject.transform.position = whiteHole[i].transform.position;
+			//rb.detectCollisions = false;
+		}
+		if (col.gameObject.tag == "finish") {
+			rb.transform.position = new Vector3(col.transform.position.x, rb.transform.position.y, col.transform.position.z);
+			gameOver = true;
+			rb.velocity = Vector3.zero;
+			rb.useGravity = false;
+			foreGround.SetActive (true);
+			text.text = "You win!\nPress 'Space' to restart";
+		}
+	}
+}
---
+++ Assets/Scripts/boardMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boardMovement : MonoBehaviour {
+	public static boardMovement Instance{ set; get;}
+
+	private Rigidbody rb;
+	public float factor;
+	public float rotationFactor;
+	public GameObject rotatingWall;
+
+	private void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+	private void Update(){
+
+		rotatingWall.transform.Rotate (0f, -rotationFactor * Time.deltaTime, 0f);
+
+		if (Input.GetKey (KeyCode.LeftArrow) && (transform.rotation.eulerAngles.z <= 20f || transform.rotation.eulerAngles.z >= 330f)) {
+			transform.Rotate (0f, 0f, factor*Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKey (KeyCode.RightArrow) && (transform.rotation.eulerAngles.z >= 340f || transform.rotation.eulerAngles.z <= 30f)) {
+			transform.Rotate (0f, 0f, -factor*Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKey (KeyCode.UpArrow) && (transform.rotation.eulerAngles.x >= 330f || transform.rotation.eulerAngles.x <= 20f)) {
+			transform.Rotate (factor*Time.deltaTime, 0f, 0f, Space.Self);
+		}
+		if (Input.GetKey (KeyCode.DownArrow) && (transform.rotation.eulerAngles.x >= 340f || transform.rotation.eulerAngles.x <= 30f)) {
+			transform.Rotate (-factor*Time.deltaTime, 0f, 0f, Space.Self);
+		}
+	
+	}
+}
